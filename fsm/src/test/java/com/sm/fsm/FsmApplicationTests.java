8b8339460918530d70f924a85a5dc422be70b235
김원규 <wonkkyuu@gmail.com>
2020-04-12 16:15:28 +0900

    프로젝트 최초 등록

---
+++ fsm/src/test/java/com/sm/fsm/FsmApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sm.fsm;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FsmApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
