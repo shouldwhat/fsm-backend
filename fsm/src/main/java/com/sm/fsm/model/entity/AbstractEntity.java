f62dc254eae37898222c2cccb956675e404865a7
김원규 <wonkkyuu@gmail.com>
2020-04-15 15:01:36 +0900

    엔티티 속성 공통 적용
    *. createTime
    *. updateTime

--- fsm/src/main/java/com/sm/fsm/model/entity/AbstractEntity.java
+++ fsm/src/main/java/com/sm/fsm/model/entity/AbstractEntity.java
@@ -1,16 +1,29 @@
 package com.sm.fsm.model.entity;
 
+import java.time.LocalDateTime;
+
+import javax.persistence.EntityListeners;
 import javax.persistence.GeneratedValue;
 import javax.persistence.Id;
 import javax.persistence.MappedSuperclass;
 
 import org.hibernate.annotations.GenericGenerator;
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.annotation.LastModifiedDate;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
 
 @MappedSuperclass
-public class AbstractEntity {
+@EntityListeners(AuditingEntityListener.class)
+public abstract class AbstractEntity {
 	
 	@Id
 	@GeneratedValue(generator = "uuid2")
 	@GenericGenerator(name = "uuid2", strategy = "uuid2")
 	private String id;
+	
+	@CreatedDate
+	private LocalDateTime createTime;
+	
+	@LastModifiedDate
+	private LocalDateTime updateTime;
 }
